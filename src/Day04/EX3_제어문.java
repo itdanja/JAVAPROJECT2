38d3e29ca0913bfeebd122e38cd7509c4ea30b5a
501t <itdanja@kakao.com>
2022-11-03 21:51:21 +0900

    DAY04 문제2 /  제어문

--- src/Day04/EX3_제어문.java
+++ src/Day04/EX3_제어문.java
@@ -51,19 +51,63 @@ public class EX3_제어문 { // c s
             System.out.println("false이다");
         } // else e
 
-        // 6. 입력받은 값이 90보다 크면 합격 아니면 불합격
         Scanner scanner = new Scanner(System.in);
+        // 6. 입력받은 값이 90보다 크면 합격 아니면 불합격
         System.out.print("점수 : ");
         int 점수 = scanner.nextInt();
         if( 점수 >= 90 ) System.out.println("합격");
         else System.out.println("불합격");
 
         // 문제1 : 입력받은 값이 홀수이면 '홀수입니다'출력 아니면 '짝수입니다' 출력
-
+        System.out.print("문제1 입력 : ");
+        int 문제1 = scanner.nextInt();
+        if( 문제1 % 2 == 1 ) System.out.println("홀수입니다.");
+        else System.out.println("짝수입니다.");
         // 문제2 : 입력받은 값이 7배수이면 '7배수입니다'출력 아니면 '7배수 아닙니다'출력
+        System.out.println("문제2 입력 : ");
+        int 문제2 = scanner.nextInt();
+        if( 문제2 % 7 == 0 ) System.out.println("7배수입니다.");
+        else System.out.println("7배수 아닙니다.");
 
         // 문제3 : 국어 , 영어 , 수학  3개의 정수를 입력받아
             // 평균이 90점 이상이면 '합격'  아니면 '불합격' 출력
+        System.out.print("국어 : "); int 국어 = scanner.nextInt();
+        System.out.print("영어 : "); int 영어 = scanner.nextInt();
+        System.out.print("수학 : "); int 수학 = scanner.nextInt();
+        int 평균 = (국어+영어+수학)/3;
+        if( 평균 >= 90 ) System.out.println("합격");
+        else System.out.println("불합격");
+
+        // 문제4 : 2개의 정수를 입력받아 더 큰수 출력
+        System.out.print("정수1 : "); int 정수1 = scanner.nextInt();
+        System.out.print("정수2 : "); int 정수2 = scanner.nextInt();
+        if( 정수1 > 정수2 ) System.out.println( 정수1 + " 더 큽니다. ");
+        else System.out.println( 정수2 + " 더 큽니다. ");
+
+        // 문제5 : 3개의 정수를 입력받아 가장 큰수 출력
+        System.out.print("숫자1 : "); int 숫자1 = scanner.nextInt();
+        System.out.print("숫자2 : "); int 숫자2 = scanner.nextInt();
+        System.out.print("숫자3 : "); int 숫자3 = scanner.nextInt();
+        int max = 숫자1;  // max 변수에 첫번째 값을 대입한다.
+        if ( max < 숫자2 ) max = 숫자2;
+        if ( max < 숫자3 ) max = 숫자3;
+        System.out.println("가장 큰수 : " + max );
+
+        // 문제6 : 3개의 정수를 입력받아 오름차순 출력  [ 3 2 5  -> 2 3 5 ]
+            // 예시
+            /*  스왑[교체]
+                    int 변수1 = 3
+                    int 변수2 = 5
+                           1. 변수1 = 변수2 [ 대입 했을때 ]
+                                변수1 = 5 / 변수2 = 5
+                    int 임시변수
+                            임시변수 = 변수1
+                                임시변수 = 3 / 변수1 = 3 / 변수2 = 5
+                            변수1 = 변수2
+                                임시변수 = 3 / 변수2 = 5 / 변수2 = 5
+                            변수2 = 임시변수
+                                임시변수 = 3 / 변수2 = 5 / 변수2 = 3
+             */
 
     } // m e
 }  // c e
